6325b0928a72c8e207ce78125c21ac974c3554cd
MargaretOwusuAgyapaa121 <margarermensah123@gmail.com>
2025-09-16 22:18:21 +0100

    update config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Blessing-Gate', // important for correct asset paths
+  base: '/blessing-gate', // important for correct asset paths
 });
